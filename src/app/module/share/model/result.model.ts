0153dec590e54f66ab092ab13c3fa128ac10bb02
linzequan <839860904@qq.com>
2022-10-25 18:02:48 +0800

    123

--- src/app/module/share/model/result.model.ts
+++ src/app/module/share/model/result.model.ts
@@ -40,3 +40,62 @@ export interface Pagination {
     recordCount?: number; 
     startRow?: number;
 }
+// 手册产品详情
+export interface ResultDetail {
+    companyId: string;
+    companyName: string;
+    configIds?: string;
+    configList?: Config[];
+    contentIds?: string;
+    contentList: Content[];
+    id: string;
+    img?: string;
+    introduction?: string;
+    manualName: string;
+    manualNumber?: number;
+    manualSerie?: string;
+    modelIds?: string;
+    modelList: TypeModel[];
+    parameterList?: [];
+    pproductCode: string;
+    productCode: string;
+    productName: string;
+    pproductName: string;
+}
+// 图片文件
+export interface Content {
+    filePath: string;
+    fileType?: string;
+    fullName?: string;
+    storeName?: string;
+}
+// 产品内部类型
+export interface TypeModel {
+    configList?: Config[];
+    id: string;
+    manualId: string;
+    modelName: string;
+    serialNumber: number;
+}
+// 配置类型
+export interface Config {
+    name: string;
+    multi: boolean;
+    isMulti: boolean;
+    id: string;
+    hostId: string;
+    serialNumber?: number;
+    configvalueList?: ConfigValue[]
+    // 自定义集合
+    hostGroup?: string[];
+} 
+// 配置参数详情
+export interface ConfigValue {
+    configId: string;
+    hostId: string;
+    id: string;
+    serialNumber: number;
+    value: string;
+    // 自定义集合
+    hostGroup?: string[]
+}
